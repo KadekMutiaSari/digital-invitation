b599097b7ce45af7b68cb70918b7acf630949263
Mutia <146809764+KadekMutiaSari@users.noreply.github.com>
2023-11-17 23:17:26 +0800

    script.js

--- js/script.js
+++ js/script.js
@@ -12,26 +12,3 @@ window.onscroll = () =>
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
-var swiper = new Swiper(".home-slider", 
-{
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect:
-    {
-        rotate: 0px,
-        stretch: 0px,
-        depth: 100px,
-        modifier: 2px,
-        slideShadows: true,
-    },
-    loop: true,
-    autoplay:
-    {
-        delay: 3000px,
-        disableOnInteraction: false,
-
-    }
-}
