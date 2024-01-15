b35f3c812cc1d55d035bcb46921421b41e6a5774
Mutia <146809764+KadekMutiaSari@users.noreply.github.com>
2024-01-16 03:13:39 +0800

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Digital Invitation</title>
+        
+        <!-- logo icon -->
+        <link rel="icon" type="image/x-icon" href="images/di_icon.png">
+        
+        <!-- custom css file link -->
+        <link rel="stylesheet" href="admin.css">
+    </head>
+    
+    <body>
+    <header>
+      <h2>Halaman Admin</h2>
+    </header>
+
+    <section>
+      <nav>
+        <ul>
+          <li><a href="#"><b>Data Admin</b></a></li>
+          <li><a href="#"><b>Data Pelanggan</b></a></li>
+          <li><a href="#"><b>Data Paket</b></a></li>
+          <li><a href="#"><b>Data Pesanan</b></a></li>
+        </ul>
+      </nav>
+
+      <article>
+        <h1>Tentang Halaman Admin</h1>
+        <p>Terdapat beberapa halaman yang hanya dapat diakses oleh Admin, seperti:</p>
+        <ol>
+          <li>Data Admin</li>
+          <li>Data Pelanggan</li>
+          <li>Data Paket</li>
+          <li>Data Pesanan</li>
+        </ol>
+        <p>
+          Gunakan menu navigasi di sebelah kiri untuk mengakses halaman-halaman tersebut.
+        </p>
+      </article>
+    </section>
+
+    <footer>
+        <div class="credit">created by <span>mutiaaasss</span> | © 2023</div>
+    </footer>
+  </body>
+</html>
