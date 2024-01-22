3926e88686f1f8df2b08391ed5cf2784f998ccc8
Mutia <146809764+KadekMutiaSari@users.noreply.github.com>
2024-01-23 06:41:30 +0800

    Update tabel paket dan pesanan

--- misc/db.sql
+++ misc/db.sql
@@ -4,24 +4,29 @@ CREATE TABLE admin (
     id_admin int(5) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nama_admin char(100) NOT NULL,
     email_admin varchar(50) NOT NULL,
+    password_admin varchar(10) NOT NULL,
     no_hp_admin varchar(15) NOT NULL);
 
 CREATE TABLE pelanggan (
     id_pelanggan int(5) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nama_pelanggan char(100) NOT NULL,
     email_pelanggan varchar(50) NOT NULL,
+    password_pelanggan varchar(10) NOT NULL,
     no_hp_pelanggan varchar(15) NOT NULL);
 
 CREATE TABLE paket (
     id_paket int(5) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nama_paket varchar(50) NOT NULL,
-    harga_paket int(10) NOT NULL);
+    harga_paket int(10) NOT NULL,
+    deskripsi_paket varchar(300) NOT NULL);
 
 CREATE TABLE pesanan (
     id_pesanan int(5) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    total_pesanan int(10) NOT NULL,
+    id_pelanggan int(5) NOT NULL,
+    id_paket int(5) NOT NULL,
     tgl_pesanan date NOT NULL,
     note_pesanan varchar(200) NOT NULL,
-    FOREIGN KEY (id_admin) REFERENCES admin(id_admin)
-    FOREIGN KEY (id_pelanggan) REFERENCES pelanggan(id_pelanggan)
-    FOREIGN KEY (id_paket) REFERENCES paket(id_paket));
+    status_pesanan varchar(20) NOT NULL,
+    FOREIGN KEY (id_pelanggan) REFERENCES pelanggan(id_pelanggan),
+    FOREIGN KEY (id_paket) REFERENCES paket(id_paket)
+);
