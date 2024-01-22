7648a2d143c83f6461aef5d7bf3f8c8a56d8f110
Mutia <146809764+KadekMutiaSari@users.noreply.github.com>
2024-01-23 06:45:39 +0800

    Update isi dalam laman konfirm_pesanan

--- konfirm_pesanan.php
+++ konfirm_pesanan.php
@@ -3,7 +3,7 @@ session_start();
 
 // Memastikan bahwa ada informasi pesanan yang disimpan di sesi
 if (!isset($_SESSION['pesanan'])) {
-    header("Location: pesanan.php");
+    header("Location: pesan.php");
     exit();
 }
 
@@ -22,20 +22,20 @@ $nomor_whatsapp = '6289524655346';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Konfirmasi Pesanan</title>
     <link rel="icon" type="image/x-icon" href="images/di_icon.png"> <!-- logo icon -->
-    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="input.css">
 </head>
 <body>
     <h2>Konfirmasi Pesanan</h2><br>
 
     <div class="container">
         <p>Pesanan Anda berhasil!</p>
-        <p>Segera hubungi kami melalui WhatsApp:</p>
-        <p><strong>Nomor WhatsApp:</strong> <?php echo $nomor_whatsapp; ?></p>
+        <p>Segera hubungi kami melalui WhatsApp</p>
+        <p>Nomor WhatsApp: <?php echo $nomor_whatsapp; ?></p>
 
-        <a href="https://wa.me/<?php echo $nomor_whatsapp; ?>?text=Halo,%20saya%20tertarik%20dengan%20pesanan%20saya%20(<?php echo $pesanan['paket']; ?>)%20dan%20saya%20menginginkan%20info%20lebih%20lanjut."
-            target="_blank">
+        <a href="https://wa.me/<?php echo $nomor_whatsapp; ?>?text=Halo,%20saya%20telah%20melakukan%20pemesanan%20(<?php echo $pesanan['paket']; ?>)%20dan%20saya%20menginginkan%20info%20lebih%20lanjut." target="_blank">
             <button>Contact via WhatsApp</button>
-        </a>
+        </a><br>
     </div>
+
 </body>
 </html>
